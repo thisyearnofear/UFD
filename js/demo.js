5588756897a7d9682645c9afde28e17c69b0cdb0
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-01-25 02:38:20 +0700

    mobile optimisation v1

--- js/demo.js
+++ js/demo.js
@@ -4,16 +4,23 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2017, Codrops
  * http://www.codrops.com
  */
 {
-	setTimeout(() => document.body.classList.add('render'), 60);
-	const navdemos = document.querySelectorAll('nav.demos > .demo');
-	Array.from(navdemos).forEach(link => link.addEventListener('click', (ev) => {
-		ev.preventDefault();
-		document.body.classList.remove('render');
-		document.body.addEventListener('transitionend', () => window.location = ev.target.href);
-	}));
+  setTimeout(() => document.body.classList.add("render"), 30);
+  const navdemos = document.querySelectorAll("nav.demos > .demo");
+  Array.from(navdemos).forEach((link) =>
+    link.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      const href = ev.currentTarget.getAttribute("href");
+      if (href) {
+        document.body.classList.remove("render");
+        setTimeout(() => {
+          window.location.href = href;
+        }, 100);
+      }
+    })
+  );
 }
